ac74f3882a914dbb30027e06b3c3e8458232ec8c
Ethan <119145352+Surxe@users.noreply.github.com>
2024-12-28 16:27:54 -0600

    loguru init, prepare read-current to be called by main.py

--- src/utils/parameters.py
+++ src/utils/parameters.py
@@ -8,6 +8,9 @@ class Parameters:
         self.params = dict()
         self.params['BOT_WIKI_USER'] = os.getenv('BOT_WIKI_USER', None)
         self.params['BOT_WIKI_PASS'] = os.getenv('BOT_WIKI_PASS', None)
+        self.params['VERBOSE'] = os.getenv('VERBOSE', True)
+
+        self.truthy = ['true', 'True', 'TRUE', '1', True]
 
     def _validate_params(self):
         for key, value in self.params.items():
@@ -16,4 +19,11 @@ class Parameters:
                 return False
     
     def get_param(self, param):
-        return self.params.get(param, f'Parameter {param} not found')
+        param_value = self.params.get(param, None)
+        if param_value is None:
+            raise ValueError(f'Parameter {param} not found')
+        return param_value
+    
+    def is_truthy(self, param):
+        param_value = self.get_param(param)
+        return param_value in self.truthy
