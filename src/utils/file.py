eac17e908b9eb3d110793e39ad6ec3df54e2802a
Ethan <119145352+Surxe@users.noreply.github.com>
2024-12-28 22:11:15 -0600

    get_blueprint_pages() init; get_prefixed_page_names() init; empty_dir() simplification

--- src/utils/file.py
+++ src/utils/file.py
@@ -1,15 +1,11 @@
 import os
+import shutil
 
 def empty_dir(path):
     """Empties a directory"""
     if not os.path.exists(path):
         return
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            os.remove(os.path.join(root, file))
-        for dir in dirs:
-            os.rmdir(os.path.join(root, dir))
-    return
+    shutil.rmtree(path)
 
 def validate_dir(path):
     """Validates that a directory exists, if not creates it"""
